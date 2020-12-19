06f30a5eac32ac2f87e692b1e7700acb654107b7
Ziad Zebib <zebibziad@gmail.com>
2020-12-19 01:27:09 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+first_number = 2.to_i
+
+second_number = 2.to_i
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
